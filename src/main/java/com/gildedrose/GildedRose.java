a84680b4f99b3a749a9d0479bc8c36cf09f89204
Nathan Theron <nathantheron@wifi-172-23-23-216.uoa-wifi.auckland.ac.nz>
2025-08-14 14:49:26 +1200

    changed some nested if statements to single if statements with && operator

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -8,25 +8,22 @@ class GildedRose {
         this.items = items;
     }
 
+
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert") 
+                    && items[i].quality > 0 && !items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                         items[i].quality = items[i].quality - 1;
                     }
-                }
             } else {
                 if (items[i].quality < maxquality) {
                     items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < maxquality) {
+                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert") && (items[i].sellIn < 11)
+                        && (items[i].sellIn) >= 6 && (items[i].quality < maxquality)) {
                                 items[i].quality = items[i].quality + 1;
                             }
-                        }
 
                         if (items[i].sellIn < 6) {
                             if (items[i].quality < maxquality) {
@@ -59,5 +56,3 @@ class GildedRose {
                 }
             }
         }
-    }
-}
